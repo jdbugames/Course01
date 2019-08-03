5ad1d355c93b192b5df90947c3d0a2e9179c1d9a
Juan Barrera <31134624+JuanBarreraU@users.noreply.github.com>
2019-08-03 17:37:42 -0500

    Day01

---
+++ Assets/Main/Scripts/DataSynchronizationMethod1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DataSynchronizationMethod1 : MonoBehaviour, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
