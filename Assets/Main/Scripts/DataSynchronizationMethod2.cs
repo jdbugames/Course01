47a5e87e82159d897dff78ffb884cf494b2d9bde
Juan Barrera <31134624+JuanBarreraU@users.noreply.github.com>
2019-08-07 14:34:48 -0500

    Day02

---
+++ Assets/Main/Scripts/DataSynchronizationMethod2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DataSynchronizationMethod2 : MonoBehaviour
+{
+    PhotonView pv;
+
+
+    private void Start ()
+    {
+        pv = GetComponent<PhotonView>();
+	}
+	
+
+	private void FixedUpdate ()
+    {
+		if(pv.IsMine && Input.GetKeyDown(KeyCode.F))
+        {
+            pv.RPC("PruebaRPC", RpcTarget.AllViaServer);
+        }
+	}
+
+    [PunRPC]
+    private void PruebaRPC()
+    {
+        if(transform.localScale.x > 1)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else
+        {
+            transform.localScale = Vector3.one * 1.5f;
+        }
+    }
+}
---
+++ Assets/Main/Scripts/DataSynchronizationMethod3.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class DataSynchronizationMethod3 : MonoBehaviour, IOnEventCallback
+{
+
+    private void Start ()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+	}
+	
+
+	private void FixedUpdate ()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            PhotonNetwork.RaiseEvent((byte)5, null, new Photon.Realtime.RaiseEventOptions { Receivers = Photon.Realtime.ReceiverGroup.All }, new ExitGames.Client.Photon.SendOptions() { });
+        }
+        
+	}
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if(photonEvent.Code == 5)
+        {
+            foreach(PhotonView pv in PhotonNetwork.PhotonViews)
+            {
+                if(pv.IsMine && pv.gameObject.CompareTag("Player"))
+                {
+                    PhotonNetwork.Instantiate("Cube", new Vector3(pv.gameObject.transform.position.x, pv.gameObject.transform.position.y + 2, pv.gameObject.transform.position.z), Quaternion.identity);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+
+}
