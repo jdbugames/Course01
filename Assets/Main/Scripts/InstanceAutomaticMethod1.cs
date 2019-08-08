b2b245971b292e59a05d584013979bd74e731e39
Juan Barrera <31134624+JuanBarreraU@users.noreply.github.com>
2019-08-07 19:23:01 -0500

    Day02

---
+++ Assets/Main/Scripts/InstanceAutomaticMethod1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class InstanceAutomaticMethod1 : MonoBehaviour
+{
+    public KeyCode tecla = KeyCode.F;
+    public string objetoInstanciado = "Cube";
+
+    private PhotonView pv;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        pv = GetComponent<PhotonView>();
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if(pv.IsMine && Input.GetKeyDown(tecla))
+        {
+            GameObject go = PhotonNetwork.Instantiate(objetoInstanciado, new Vector3(pv.gameObject.transform.position.x, pv.gameObject.transform.position.y + 2, pv.gameObject.transform.position.z), Quaternion.identity);
+            print(string.Concat("Se ha instanciado de forma automática el objeto ", go.name, "con ViewID", go.GetPhotonView().ViewID));
+        }
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<PhotonView>() != null)
+        {
+            PhotonNetwork.Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Main/Scripts/InstanceManualMethod1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class InstanceManualMethod1 : MonoBehaviour
+{
+    public KeyCode tecla = KeyCode.F;
+    public GameObject objetoAInstanciar;
+
+    private PhotonView pv;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        pv = GetComponent<PhotonView>();
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if(pv.IsMine && Input.GetKeyDown(tecla))
+        {
+            pv.RPC("InstanciarObjeto", RpcTarget.AllViaServer);
+        }
+		
+	}
+
+    [PunRPC]
+    private void InstanciarObjeto()
+    {
+        GameObject instancia = Instantiate(objetoAInstanciar, new Vector3(pv.gameObject.transform.position.x, pv.gameObject.transform.position.y + 2, pv.gameObject.transform.position.z), Quaternion.identity);
+        PhotonNetwork.AllocateViewID(instancia.GetPhotonView());
+        print(string.Concat("Se ha instanciado de forma automática el objeto ", instancia.name, "con ViewID", instancia.GetPhotonView().ViewID));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<PhotonView>() != null)
+        {
+            PhotonNetwork.Destroy(collision.gameObject);
+        }
+    }
+}
